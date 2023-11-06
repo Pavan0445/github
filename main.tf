630d8a825188862b77137048438d3459f2d44fd6
Pavan0445 <95559407+Pavan0445@users.noreply.github.com>
2023-11-05 22:36:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "web_server" {
 }
 
 resource "aws_security_group" "My_Monday_Task" {
-  name        = "http-https-ssh-sg123456789012"
+  name        = "http-https-ssh-sg1234567890123"
   description = "Allow HTTP, HTTPS, and SSH traffic"
 }
 
